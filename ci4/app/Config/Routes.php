b1d658e0740fe9f3ec7f01463fb1c835c468da59
Althea Noelle Sarmiento <absarmiento@student.apc.edu.ph>
2024-03-04 11:33:45 +0800

    created guest pages

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -1,19 +1,25 @@
 <?php
-
+ 
 use CodeIgniter\Router\RouteCollection;
-
+ 
 /**
- * @var RouteCollection $routes
- */
+* @var RouteCollection $routes
+*/
 $routes->get('/', 'Home::index');
-
-use App\Controllers\News; // Add this line
+ 
+use App\Controllers\Guests; 
+use App\Controllers\News; 
 use App\Controllers\Pages;
-
+ 
+$routes->get('guests', [Guests::class, 'index']);           // Add this line
+$routes->get('guests/(:segment)', [Guests::class, 'show']); // Add this line
+ 
+ 
 $routes->get('news', [News::class, 'index']);  
-$routes->get('news/new', [News::class, 'new']); // Add this line
-$routes->post('news', [News::class, 'create']); // Add this line
-$routes->get('news/(:segment)', [News::class, 'show']); 
-
+$routes->get('news/new', [News::class, 'new']); // Add this line	
+$routes->post('news', [News::class, 'create']); // Add this line        
+$routes->get('news/(:segment)', [News::class, 'show']);
+ 
+ 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
